72afdf52acfa3600db37e421a7f7eb6f4a729143
kg0529 <qq610546847@gmail.com>
2021-03-04 11:35:00 -0500

    add check cpu

---
+++ check-cpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# partial code coming from https://unix.stackexchange.com/questions/417209/get->
+# from usermazs
+
+a=1
+while [  $a -lt 10 ]
+do
+  cores=$(nproc) 
+  load=$(awk '{print $3}'< /proc/loadavg)
+  echo | awk -v c="${cores}" -v l="${load}" '{print " " l*100/c "%"}'
+  usage=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F.>
+  if [[ ${usage} -ge 60 ]]; then
+     if [[ ${usage} -ge 80 ]]; then
+         echo "Alert"
+     else
+         echo "Warning"
+     fi
+  fi
+  echo "------------------------"
+  sleep 2
+done
